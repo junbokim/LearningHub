c1c3cfa6ae314f09f2c45efe7af440307941d6e4
junbokim <junbojoshuakim@gmail.com>
2021-03-05 18:35:13 +1100

    Partials and ejs practices

---
+++ Web Dev/Express/Partials/app.js
@@ -0,0 +1,21 @@
+const { Console } = require("console");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname,"/views"))
+
+
+app.get("/",(req,res)=>{
+    res.render("index", {title : "home"})   
+})
+
+app.get("/aboutMe",(req,res)=>{
+    res.render("aboutme", {title : "home"})   
+})
+
+app.listen(8080,()=>{
+    console.log("Server Is Live!")
+})
